a0710e65ca8323a993ea3e53376df8d0acd133f2
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-01-21 16:07:56 +0100

    Refactoring (new hoc and hooks)

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,7 +3,15 @@ import styles from './styles.module.css';
 const Categories = ({ categories, setSelectedCategory, selectedCategory }) => {
     return (
         <div className={styles.categories}>
+            <button onClick={() => setSelectedCategory(null)}
+                className={
+                    !selectedCategory ? styles.active : styles.item
+                }
+            >
+                All
+            </button>
             {categories.map(category => {
+
                 return (
                     <button onClick={() => setSelectedCategory(category)}
                         className={
