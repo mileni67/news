af707e5be14dfe735fe35be17af8ede5f5a9c14d
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-01-23 20:51:45 +0100

    add design for web

--- src/components/NewsBanner/NewsBanner.jsx
+++ src/components/NewsBanner/NewsBanner.jsx
@@ -1,5 +1,4 @@
 import { formatTimeAgo } from "../../helpers/formatTimeAgo";
-import withSkeleton from "../../helpers/hocs/withSkeleton";
 import Image from "../Image/Image";
 import styles from './styles.module.css';
 
@@ -13,6 +12,4 @@ const NewsBanner = ({ item }) => {
     );
 };
 
-const NewsBannerWithSkeleton = withSkeleton(NewsBanner, 'banner', 1);
-
-export default NewsBannerWithSkeleton;
+export default NewsBanner;
