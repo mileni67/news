c051d7e89ca504c10fb23082ad05823c28437289
Oleksandr Vykhor <sashavihor67@gmail.com>
2024-06-27 20:49:56 +0200

    add news page and react router

--- src/app/appEntry.tsx
+++ src/app/appEntry.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./appStore";
-import BaseLayout from "./layouts/BaseLayout";
+import { RouterProvider } from "react-router-dom";
 import "@/shared/index.css";
 import { ThemeProvider } from "./providers/ThemeProvider";
+import { appRouter } from "./appRouter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider>
       <Provider store={store}>
-        <BaseLayout />
+        <RouterProvider router={appRouter} />
       </Provider>
     </ThemeProvider>
   </React.StrictMode>
